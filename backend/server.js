036b3237804aa649f2f7a72c0f965cfb7fb6f1a7
Sarah-faisal <sarahpervaizpk@gmail.com>
2023-08-24 21:54:09 +0500

    mongo connected

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
 import {notFound , errorHandler} from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
 
 //to make the use of .env file
 dotenv.config();
 const port = process.env.PORT || 5000;
 
+connectDB();
 const app = express();
 
 app.use('/api/users', userRoutes);
