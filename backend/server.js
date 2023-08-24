6125608df9b234c41003da1e8da46fe074099eac
Sarah-faisal <sarahpervaizpk@gmail.com>
2023-08-24 18:33:16 +0500

    the home server is connected

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import userRoutes from './routes/userRoutes.js';
+// import userRoutes from './routes/userRoutes.js';
+//to make the use of .env file
 dotenv.config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use('/api/users', userRoutes);
+// app.use('/api/users', userRoutes);
 // adding route_ callback function
 app.get('/' , (req , res)=>res.send('Server is ready'));
 app.listen(port , ()=>console.log(`Server started on the port ${port}`));
