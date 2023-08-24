e4b24970ab097b03cde981d3caea7445e4516bbe
Sarah-faisal <sarahpervaizpk@gmail.com>
2023-08-24 20:07:39 +0500

    .

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,8 +2,8 @@ import express from "express";
 const router = express.Router();
 import {authUser , registerUser , logoutUser , getUserProfile , updateUserProfile} from "../controllers/userControllers.js";
 
-router.post('/' , authUser);
-router.post('/auth' , registerUser);
+router.post('/' , registerUser);
+router.post('/auth' , authUser);
 router.post('/logout' , logoutUser);
 router.route('/profile').get(getUserProfile).put(updateUserProfile);
 export default router;
