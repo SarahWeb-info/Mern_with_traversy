45b4f200f424bfe3876fbc0be66c4e61c9c0357e
Sarah-faisal <sarahpervaizpk@gmail.com>
2023-08-26 16:41:19 +0500

    all the features register,login,logout,update done

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -65,12 +65,37 @@ const logoutUser =asyncHandler( async (req , res) =>{
 // route  GET /api/users
 // Access : Private
 const getUserProfile =asyncHandler( async (req , res) =>{
-    res.status(200).json({message : 'User Profile'})
+    const user = {
+        _id : req.user._id,
+        name : req.user.name,
+        email: req.user.email   
+    }
+    res.status(200).json(user);
 });
 // desc:    Update user profile
 // route  PUT /api/users
 // Access : Private
 const updateUserProfile =asyncHandler( async (req , res) =>{
+    const user = await User.findOne(req.user._id);
+    if(user){
+        user.name = req.user.name || user.name;
+        user.email=req.user.email || user.email  ;
+        if(req.body.password){
+            user.password = req.body.password;
+        }
+        const updatedUser = await user.save();
+
+        res.status(200).json({
+            _id : updatedUser._id,
+            name : updatedUser.name,
+            email: updatedUser.email   
+        });
+        
+    }else{
+        res.status(404);
+        throw new Error('No such User found');
+    }
+
     res.status(200).json({message : 'Update User Profile'})
 });
 export {authUser , registerUser , logoutUser , getUserProfile , updateUserProfile};
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
 import {notFound , errorHandler} from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
-
+import cookieParser from 'cookie-parser';
 //to make the use of .env file
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -12,6 +12,8 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
 app.use('/api/users', userRoutes);
 
 // adding route_ callback function
