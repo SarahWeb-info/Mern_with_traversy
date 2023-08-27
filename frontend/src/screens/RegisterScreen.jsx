51318d2b7ebf869a602e20c78f56f4c5fc0369cf
Sarah-faisal <sarahpervaizpk@gmail.com>
2023-08-27 21:06:34 +0500

    sign up page done

--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -4,9 +4,11 @@ import Form from 'react-bootstrap/Form';
 import FormContainer from '../components/formContainer';
 import {Link} from 'react-router-dom';
 
-function LoginScreen() {
+function RegisterScreen() {
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [confirmPassword, setconfirmPassword] = useState('');
 
     const submitHandler = () =>{
         e.preventDefault();
@@ -14,8 +16,12 @@ function LoginScreen() {
     } 
   return (
     <FormContainer>
-     <h1>Sign In</h1>   
+     <h1>Sign Up</h1>   
     <Form onSubmit={submitHandler}>
+      <Form.Group className="mb-3" controlId="name">
+        <Form.Label>Enter Name</Form.Label>
+        <Form.Control type="text" placeholder="Enter Name" value={name} onChange={(e)=>setName(e.target.value)} />
+      </Form.Group>  
       <Form.Group className="mb-3" controlId="email">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e)=>setEmail(e.target.value)} />
@@ -25,13 +31,18 @@ function LoginScreen() {
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)} />
       </Form.Group>
+      <Form.Group className="mb-3" controlId="confirmPassword">
+        <Form.Label>Confirm Password</Form.Label>
+        <Form.Control type="password" placeholder="Confirm Password" value={confirmPassword} onChange={(e)=>setconfirmPassword(e.target.value)} />
+      </Form.Group>
+
       <Button variant="primary" type="submit">
-        Sign In
+        Sign Up
       </Button>
-      <Link to='/register' className="mx-3">Register?</Link>
+      Already have an account? <Link to='/login' className="mx-3">Login</Link>
     </Form>
     </FormContainer>
   );
 }
 
-export default LoginScreen;
+export default RegisterScreen;
